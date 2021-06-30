9638f9c2a6d2dac4b558fc6a5b6f08f727ae286d
daniele frisanco <ottoct@gmail.com>
2021-06-30 11:47:30 +0200

    Added jQuery

--- app/controllers/qr_code_link_controller.rb
+++ app/controllers/qr_code_link_controller.rb
@@ -30,9 +30,7 @@ class QrCodeLinkController < ApplicationController
   # This action save the ndef_message
   def save_ndef_message
     qr_code_link = QrCodeLink.find(params_id)
-    puts '----------------------------------------------------------------'
-    puts params
-    if qr_code_link.ndef_messages.create(message: params_ndef_message[:message], serial_number: params_ndef_message[:serial_number])
+    if qr_code_link.ndef_messages.create(params_ndef_message)
       redirect_to :show_path, id: params_id
     else
       render status: 500
